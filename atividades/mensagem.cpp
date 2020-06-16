1040e6e1354201e63224c8f4457cee60d097730a
Lucas Fernandes <51387908+lucandes@users.noreply.github.com>
2020-06-16 17:51:23 -0300

    Correção

--- atividades/mensagem.cpp
+++ atividades/mensagem.cpp
@@ -2,14 +2,14 @@
 #include <string>
 
 int main(int argc, char *argv[]){
-	std::string msg[2];
+	std::string msg;
 	if (argc == 1 || std::string(argv[1]) != "add"){
 		std::cout << "Uso: " << argv[0] << " add <mensagem>" << std::endl;
 		return 1;
 	}
 	if (argc == 2 && std::string(argv[1]) == "add"){
 		std::cout << "Insira a mensagem: ";
-		std::getline(std::cin, msg[1]);
+		std::getline(std::cin, msg);
 	}
 
 	std::cout << "Mensagem Adicionada" << std::endl;
