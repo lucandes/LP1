2c55b5c1d5026adab65d59c1e0c3786e5c9f84dc
Lucas <lucasfern99@gmail.com>
2020-06-18 15:20:01 -0300

    Listagem de mensagens implementada

--- atividades/mensagem.cpp
+++ atividades/mensagem.cpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <fstream>
 
-void print_usage(const std::string &filename);
+void print_usage(const std::string &progname);
+int list_file(const std::string filename);
 
 int main(int argc, char *argv[]){
 	if (argc == 1){
@@ -10,31 +11,61 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	std::string action = std::string(argv[1]);
-	if (action != "add"){
-		print_usage(argv[0]);
+	std::string action = argv[1];
+	std::string msg;
+	std::string const filename = "diary.txt";
+	if (argc == 2){
+		if (action == "list"){
+			return list_file(filename);
+		}
+		if (action == "add"){
+			std::cout << "Insira a mensagem: ";
+			std::getline(std::cin, msg);
+		}
+		else {
+			print_usage(argv[0]);
+			return 1;
+		}
+	}
+
+	else {
+		if (action == "add"){
+			msg = std::string(argv[2]);
+		}
+		else {
+			print_usage(argv[0]);
+			return 1;
+		}
+	}
+
+	std::ofstream arquivo_saida(filename, std::ios::app);
+	if (arquivo_saida.is_open()){
+		arquivo_saida << msg << std::endl;
+		arquivo_saida.close();
+		std::cout << "Mensagem Adicionada" << std::endl;
+	}
+	else {
+		std::cerr << "Erro: arquivo não pode ser aberto." << std::endl;
 		return 1;
 	}
 
-	std::string msg;
-	if (argc == 2){
-		std::cout << "Insira a mensagem: ";
-		std::getline(std::cin, msg);
-	}
-
-	else if (argc > 2){
-		msg = std::string(argv[2]);
-	}
-
-	std::ofstream arquivo_saida("log.txt", std::ios::app);
-	arquivo_saida << msg << std::endl;
-	arquivo_saida.close();
-
-	std::cout << "Mensagem Adicionada" << std::endl;
 	return 0;
 }
 
 
-void print_usage(const std::string &filename){
-	std::cout << "Uso: " << filename << " add <mensagem>" << std::endl;
+void print_usage(const std::string &progname){
+	std::cout << "Uso: " << progname << " add <mensagem>" << std::endl;
+}
+
+int list_file(const std::string filename){
+	std::ifstream arquivo_entrada(filename, std::ios::in);
+	if (arquivo_entrada.fail()){
+		std::cerr << "Erro: não foi possível abrir o arquivo de mensagens." << std::endl;
+		return 1;
+	}
+	std::string line;
+	while (std::getline(arquivo_entrada, line)){
+		std::cout << line << std::endl;
+	}
+	arquivo_entrada.close();
 }
